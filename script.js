5483dc26f83c9ba0156e708892d071f41b43c8e0
Manas Chaudhary <manaschaudhary1602@gmail.com>
2025-07-04 20:02:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-//your code here
-// Define song details
-const songName = "Perfect";
-const youtubeLink = "https://www.youtube.com/watch?v=2Vv-BfVoq4g"; // Official YouTube link
 
+const songName = "Perfect";
+const youtubeLink = "https://www.youtube.com/watch?v=2Vv-BfVoq4g; 
 
 const container = document.getElementById("songContainer");
-
-
 const paragraph = document.createElement("p");
 
-// Create the <a> tag
 const anchor = document.createElement("a");
 anchor.href = youtubeLink;
 anchor.textContent = songName;
 anchor.target = "_blank"; 
 
-// Append text + anchor to paragraph
 paragraph.textContent = "My current favourite song is ";
 paragraph.appendChild(anchor);
 
-// Add paragraph to container
 container.appendChild(paragraph);
